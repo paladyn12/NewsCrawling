88e5fb93624f4bce7dd44c54f04e695586f5cac2
Taewan Kim <paladyn12@naver.com>
2024-05-22 22:20:28 +0900

    newsCrawling

---
+++ src/main/java/com/example/newsCrawling/NewsCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsCrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsCrawlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newsCrawling/NewsCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsCrawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsCrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
